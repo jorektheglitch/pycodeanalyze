f2050ef8a7eaad62cfa036f79efbb1f24f957f26
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-21 22:34:43 +0300

    Fix `analyze_file` function

--- analyst/analyze.py
+++ analyst/analyze.py
@@ -1,14 +1,16 @@
 import ast
 from pathlib import Path
 
-from .analytics import Module
+from .analytics import Module, Name
 from .analyzers import ModuleAnalyzer
 
 
-def analyze_file(path: str | Path) -> Module:
+def analyze_file(path: str | Path, name: str | None = None) -> Module:
     path = Path(path).resolve()
+    name = name or "<string>"
+    module_name = Name(tuple(name.split(".")))
     with path.open() as f:
         source = f.read()
     tree = ast.parse(source, path.name, "exec")
-    module = ModuleAnalyzer.analyze(tree, None, None)
+    module = ModuleAnalyzer.analyze(tree, None, module_name)
     return module
