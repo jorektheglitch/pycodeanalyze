8c7cee04a79e65a018c193ea049777d6d7273670
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-21 22:34:01 +0300

    Slight changes in types system

--- analyst/analytics.py
+++ analyst/analytics.py
@@ -4,54 +4,58 @@ import ast
 from abc import ABC
 from dataclasses import dataclass
 
-from typing import Any, NewType
+from typing import Any, MutableSequence, NewType
 
 Name = NewType("Name", tuple[str, ...])
 
 
 @dataclass
-class Value(ABC):
+class BaseValue(ABC):
+    parent: BaseValue | None
     type: Class | None
-    dependencies: list[Value]
+    dependencies: MutableSequence[BaseValue]
 
 
 @dataclass
-class Module(Value, dict[str, Value]):
+class Module(BaseValue, dict[str, BaseValue]):
     parent: Module | None
     name: Name
     type: Class | None
-    dependencies: list[Value]
+    dependencies: MutableSequence[BaseValue]
 
     def __init__(
         self,
         parent: Module | None,
         name: Name,
-        dependencies: list[Value],
+        dependencies: MutableSequence[BaseValue],
         *args: Any, **kwargs: Any
     ):
         from .builtin_types import module_type
         dict.__init__(self, *args, **kwargs)
-        super().__init__(module_type, dependencies)
+        super().__init__(parent, module_type, dependencies)
         self.parent = parent
         self.name = name
 
 
-class Variable(Value):
+@dataclass
+class Value(BaseValue):
     parent: Module | Function | Class
     type: Class | None
-    dependencies: list[Value]
+    dependencies: MutableSequence[BaseValue]
     definition: ast.expr
 
 
-class Function(Value):
+@dataclass
+class Function(BaseValue):
     parent: Module | Function | Class
     type: Class | None
-    dependencies: list[Value]
+    dependencies: list[BaseValue]
     definition: ast.FunctionDef
 
 
-class Class(Value, dict[str, Value]):
+@dataclass
+class Class(BaseValue, dict[str, BaseValue]):
     parent: Module | Function | Class
     type: Class | None
-    dependencies: list[Value]
+    dependencies: list[BaseValue]
     definition: ast.ClassDef
